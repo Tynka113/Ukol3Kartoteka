b2697c97920d317ead83772bafdfb455d4bb1814
Tynka113 <macha@DESKTOP-TMSSHNL>
2023-11-08 21:32:58 +0100

    Add project files.

---
+++ Ukol3Kartoteka/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+/*
+ * Zadání
+Úkol je obdobou úkolu zápisu do souboru 
+(viz. https://moje.czechitas.cz/cs/udalosti/prehled/2090-c-2/domaci-ukol/549).
+
+Jen zde se nebudou zadávat řádky, 
+ale údaje o osobě(případně o knize a podobně) jako jméno, 
+příjmení, rok narození apod. (opět dle fantazie a kreativity)
+
+Každá osoba se takto uloží do souboru. Co osoba to řádek v souboru.
+
+Uživatel by v minimálním provedení měl mít možnost přidat osobu 
+a vypsat všechny osoby. Pokud si budeš chtít vyhrát jde udělat mnoho 
+dalších funkcí jako vypsat jen osoby nějakého roku narození apod. 
+Tedy lze dát velký prostor kreativitě.
+*/
